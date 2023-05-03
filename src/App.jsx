673350c30680ebddcf436a58e2c44a57cf760422
deradhista <deradhista@gmail.com>
2023-05-03 15:42:08 +0700

    add: handle delete rows

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,31 @@ import Table from "./components/Table";
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
 
+  const [rows, setRows] = useState([
+    {
+      page: "page 1",
+      description: "this is first page",
+      status: "live",
+    },
+    {
+      page: "page 2",
+      description: "this is second page",
+      status: "draft",
+    },
+    {
+      page: "page 3",
+      description: "this is third page",
+      status: "error",
+    },
+  ]);
+
+  const handleDeleteRows = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+
   return (
     <div className="App">
-      <Table />
+      <Table rows={rows} deleteRow={handleDeleteRows} />
       <button className="btn" onClick={() => setModalOpen(true)}>
         Add
       </button>
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/Table.css";
 import { BsFillTrashFill, BsFillPencilFill } from "react-icons/bs";
 
-function Table() {
+function Table({ rows, deleteRow }) {
   return (
     <div className="table-wrapper">
       <table className="table">
@@ -15,45 +15,30 @@ function Table() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Home</td>
-            <td>This main page</td>
-            <td>
-              <span className="label label-live">Live</span>
-            </td>
-            <td>
-              <span className="actions">
-                <BsFillTrashFill className="delete-btn" />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Page 2</td>
-            <td>This page 2</td>
-            <td>
-              <span className="label label-draft">Draft</span>
-            </td>
-            <td>
-              <span className="actions">
-                <BsFillTrashFill className="delete-btn" />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
-          <tr>
-            <td>Page 3</td>
-            <td>This page third</td>
-            <td>
-              <span className="label label-error">Error</span>
-            </td>
-            <td>
-              <span className="actions">
-                <BsFillTrashFill className="delete-btn" />
-                <BsFillPencilFill />
-              </span>
-            </td>
-          </tr>
+          {rows.map((row, idx) => {
+            const statusText =
+              row.status.charAt(0).toUpperCase() + row.status.slice(1);
+            return (
+              <tr key={idx}>
+                <td>{row.page}</td>
+                <td className="expand">{row.description}</td>
+                <td>
+                  <span className={`label label-${row.status}`}>
+                    {statusText}
+                  </span>
+                </td>
+                <td>
+                  <span className="actions">
+                    <BsFillTrashFill
+                      className="delete-btn"
+                      onClick={() => deleteRow(idx)}
+                    />
+                    <BsFillPencilFill />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
